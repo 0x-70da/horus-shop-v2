edea7a954f9213db9eae5febd8706ea09f98df97
0x70da <mahmoudAnonymous0@gmail.com>
2026-03-31 19:51:25 +0200

    Refactor routing structure and integrate RouterProvider in App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
-
-const queryClient = new QueryClient()
+import { queryClient } from '@/lib/query-client'
+import { router } from './router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
 )
