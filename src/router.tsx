93cea2e32f42da320a3185c999fd446c7a863132
0x70da <mahmoudAnonymous0@gmail.com>
2026-04-02 08:06:54 +0200

    Add Layout component and update router configuration for improved structure

--- src/router.tsx
+++ src/router.tsx
@@ -1,24 +1,44 @@
 import { createBrowserRouter } from "react-router-dom";
-import GuestGuard from "./features/auth/components/guest.protect";
+import GuestGuard from "./features/auth/components/guest.guard";
 import Layout from "./components/Layout";
 import LoginPage from "./features/auth/pages/login.page";
 import RegisterPage from "./features/auth/pages/register.page";
+import AuthGuard from "./features/auth/components/auth.guard";
+import NotFound from "./pages/NotFound";
+import HomePage from "./pages/HomePage";
 
 export const router = createBrowserRouter([
     {
-        // element: <GuestGuard />,
+        element: <GuestGuard />,
         children: [
             { path: "/login", element: <LoginPage /> },
             { path: "/register", element: <RegisterPage /> },
         ]
     },
 
-    // {
-    //     path: "/",
-    //     element: <Layout />,
-    //     children: [
-    //         { index: true, element: <HomePage /> },
-    //         { path: "products", element: <ProductsPage /> },
-    //     ]
-    // }
-])
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            { index: true, element: <HomePage /> },
+            { path: "products", element: <p>Products Page</p> },
+        ]
+    },
+
+    {
+        element: <AuthGuard />,
+        children: [
+            {
+                path: "/",
+                element: <Layout />,
+                children: [
+                    { path: "cart", element: <p>Cart Page</p> },
+                    { path: "orders", element: <p>Orders Page</p> },
+                    { path: "profile", element: <p>Profile Page</p> },
+                ]
+            }
+        ]
+    },
+
+    { path: "*", element: <NotFound /> }
+]);
